9ce0c7682f0ade8f64c8d05018ddec036c128e8d
Ajay sharma <aakirtsharma123@gmail.com>
2025-03-09 02:41:51 +0545

    new change

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: config.clientUrl,
     credentials: true,
   },
 });
@@ -21,7 +21,7 @@ const io = new Server(server, {
 app.use(helmate());
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: config.clientUrl,
     credentials: true,
   })
 );
