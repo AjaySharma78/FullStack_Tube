bc477b51c4d5b388a7d2585ff1860c156e65d02d
Ajay sharma <aakirtsharma123@gmail.com>
2025-03-09 04:07:56 +0545

    new

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -174,6 +174,7 @@ const loginUsers = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: true,
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   };
 
