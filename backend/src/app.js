f47330c5bad0479543474230b06e78d9db96d05d
Ajay sharma <aakirtsharma123@gmail.com>
2025-03-09 12:33:15 +0545

    new

--- backend/src/app.js
+++ backend/src/app.js
@@ -39,8 +39,8 @@ app.use(
 );
 
 app.use(cookieParser());
-app.use(express.json({ limit: "50kb" }));
-app.use(express.urlencoded({ extended: true, limit: "50kb" }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.static("public"));
 app.use(passport.initialize());
 app.use(passport.session());
